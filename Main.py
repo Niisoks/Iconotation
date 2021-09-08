acd1e5d9e420db4fed451a2f2b9b6759ab576393
Niisoks <colebrhall@gmail.com>
2021-09-08 03:39:21 +0100

    Add images and python application
    cool

---
+++ Main.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import os
+
+path = '/home/niisoks/Programming/Python/'
+extension = '.png'
+
+imageDict = {}
+imageList = ['1','2','3','4','NULL','6','7','8','9',
+             '>','+','a','b','c','d','[','j','*',
+             'h','v','r','x']
+
+
+def open_images_into_dictionary(imageList, imageDict):
+    for x in range(len(imageList)):
+        currentMove = imageList[x]
+        imageDict[currentMove] = Image.open(path + str(x + 1) + extension)
+        print('opening:' + path + str(x + 1) + extension)
+
+def generate_image(userInput):
+    dst = Image.new('RGB', (len(userInput[0]) * 20, len(userInput) * 30)) 
+    for i in range(len(userInput)):
+        for z in range(len(userInput[i])):
+            dst.paste(imageDict[userInput[i][z]],(20 * z , 30 * i))
+
+    dst = dst.resize((dst.width * 2, dst.height * 2), resample=None, box=None, reducing_gap=None)
+    dst.save('/home/niisoks/Programming/Python/Cringe.png')
+    
+
+def generate_valid_inputs(userInput):
+    emptyList = []
+    for i in range(len(userInput)):
+        if userInput[i] in imageDict:
+            emptyList.append(userInput[i])
+        else:
+            print('command ' + userInput[i] + ' not recognised - removing')
+
+    return emptyList
+
+def chunks(L, n): return [L[x: x+n] for x in range(0, len(L), n)]
+
+open_images_into_dictionary(imageList, imageDict)
+
+userInput = list(input('insert combo').lower())
+userInput = generate_valid_inputs(userInput)
+userInput = chunks(userInput, 30)
+print(userInput)
+generate_image(userInput)
